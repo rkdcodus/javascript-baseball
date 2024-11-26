58dea8f17f45e0e2a8c7382f657d24a44817bfc2
rkdcodus <rcd7275@naver.com>
2024-11-26 16:23:57 +0900

    서로 다른 3자리 랜덤 숫자 뽑는 함수 생성 (구현 중)

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ const button = document.querySelector(".button");
 const output = document.querySelector(".box");
 
 button.addEventListener("click", () => {
-
   if (input.value == 1) {
-    output.innerText(input.value);
-
+    output.innerText = input.value;
     //컴퓨터 숫자 랜덤 생성&출력
-
+    const correctAnswer = pickRandomNumber();
+    console.log(correctAnswer);
   } else if (input.value == 9) {
     alert("애플리케이션이 종료되었습니다.");
   } else {
     alert("1 혹은 9를 입력해주세요");
   }
-
 });
+
+function pickRandomNumber() {
+  let numbers = Math.floor(Math.random() * 9) + 1;
+  while (numbers.length <= 3) {
+    const num = Math.floor(Math.random() * 9) + 1;
+    // 서로 다른 숫자 합치기
+  }
+  return numbers;
+}
