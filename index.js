1dd211b807ad93c32339aff0ed7213d6c9b30115
Next-dreamer <mjo2640@gmail.com>
2024-11-27 16:56:30 +0900

    9입력 시 output 내용 초기화, 비교 함수 조건문 수정

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ if (!correctAnswer) {
 
       output.insertAdjacentHTML("beforeend", "컴퓨터가 숫자를 뽑았습니다.<br>");
     } else if (input.value == 9) {
+      output.innerHTML = "";
       alert("애플리케이션이 종료되었습니다.");
     }
 
-    if (correctAnswer) {
+    if (correctAnswer && input.value != 1) {
       compareNumber();
+    } else if (input.value == 1) {
     } else {
       alert("1 혹은 9를 입력해주세요");
     }
@@ -24,6 +26,7 @@ if (!correctAnswer) {
   });
 }
 
+// 숫자 랜덤 뽑기 함수
 function pickRandomNumber() {
   let numbers = "";
 
@@ -38,14 +41,17 @@ function pickRandomNumber() {
   return numbers;
 }
 
+// 숫자 비교 함수
 function compareNumber() {
   let threeNumbers = input.value;
+  const set = new Set(threeNumbers);
 
-  if (input.value.length == 3) {
+  if (set.size == 3) {
     // 컴퓨터 3자릿수와 비교하고 힌트를 출력
     output.insertAdjacentHTML("beforeend", `${threeNumbers}<br>`);
     output.insertAdjacentHTML("beforeend", "숫자를 입력해주세요.<br>");
   } else {
     output.insertAdjacentHTML("beforeend", "중복없이 3자릿수를 입력해주세요.<br>");
   }
+  console.log(set);
 }
