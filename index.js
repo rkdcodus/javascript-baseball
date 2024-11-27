c8a7812a7cbb6b8fedc82948be5c9aeb443bb13b
rkdcodus <rcd7275@naver.com>
2024-11-27 15:14:30 +0900

    compareNumber 함수명 짓기, 실행(작성 중)

--- index.js
+++ index.js
@@ -2,30 +2,27 @@ const input = document.querySelector(".input");
 const button = document.querySelector(".button");
 const output = document.querySelector(".output");
 
-button.addEventListener("click", () => {
-  // let num = 1
+let correctAnswer = 0;
 
-  // if (num) {
-  //   num++
-  // } else (num == 2) {
+if (!correctAnswer) {
+  button.addEventListener("click", () => {
+    if (input.value == 1) {
+      //컴퓨터 숫자 랜덤 생성&출력
+      correctAnswer = pickRandomNumber();
 
-  // }
+      output.insertAdjacentHTML("beforeend", "컴퓨터가 숫자를 뽑았습니다.<br>");
+    } else if (input.value == 9) {
+      alert("애플리케이션이 종료되었습니다.");
+    }
 
-  if (input.value == 1) {
-    //컴퓨터 숫자 랜덤 생성&출력
-    const correctAnswer = pickRandomNumber();
-
-    output.insertAdjacentHTML("beforeend", "컴퓨터가 숫자를 뽑았습니다.<br>");
-
-  } else if (input.value == 9) {
-    alert("애플리케이션이 종료되었습니다.");
-  } else {
-    alert("1 혹은 9를 입력해주세요");
-
-  }
-
-  input.value = "";
-});
+    if (correctAnswer) {
+      compareNumber();
+    } else {
+      alert("1 혹은 9를 입력해주세요");
+    }
+    input.value = "";
+  });
+}
 
 function pickRandomNumber() {
   let numbers = "";
@@ -41,16 +38,14 @@ function pickRandomNumber() {
   return numbers;
 }
 
-function Y() {
+function compareNumber() {
   let threeNumbers = input.value;
-  output.insertAdjacentHTML("beforeend", "숫자를 입력해주세요.<br>");
 
   if (input.value.length == 3) {
     // 컴퓨터 3자릿수와 비교하고 힌트를 출력
-    // 
+    output.insertAdjacentHTML("beforeend", `${threeNumbers}<br>`);
     output.insertAdjacentHTML("beforeend", "숫자를 입력해주세요.<br>");
   } else {
     output.insertAdjacentHTML("beforeend", "중복없이 3자릿수를 입력해주세요.<br>");
   }
-
 }
