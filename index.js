9e867e3e9b01893f3cb180532013bd14a135ae95
Next-dreamer <mjo2640@gmail.com>
2024-11-27 14:18:56 +0900

    html,  js 파일 수정

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const input = document.querySelector(".input");
 const button = document.querySelector(".button");
-const output = document.querySelector(".box");
+const output = document.querySelector(".output");
 
 button.addEventListener("click", () => {
+// let num = 1
+
+// if (num) {
+//   num++
+// } else (num == 2) {
+  
+// }
   if (input.value == 1) {
     output.innerText = input.value;
     //컴퓨터 숫자 랜덤 생성&출력
     const correctAnswer = pickRandomNumber();
     console.log(correctAnswer);
+    input.value = ""
+    output.insertAdjacentText("afterend") = "컴퓨터가 숫자를 뽑았습니다.<br>"
   } else if (input.value == 9) {
     alert("애플리케이션이 종료되었습니다.");
+    input.value = ""
   } else {
     alert("1 혹은 9를 입력해주세요");
+    input.value = ""
   }
 });
 
