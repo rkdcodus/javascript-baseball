48543028524da992509313727dcc6c1dc4ebfa8f
rkdcodus <rcd7275@naver.com>
2024-11-27 14:26:05 +0900

    pickRandomNumber() 함수 구현
    서로 다른 세자리의 랜덤 숫자 뽑는 함수

--- index.js
+++ index.js
@@ -3,34 +3,39 @@ const button = document.querySelector(".button");
 const output = document.querySelector(".output");
 
 button.addEventListener("click", () => {
-// let num = 1
+  // let num = 1
 
-// if (num) {
-//   num++
-// } else (num == 2) {
-  
-// }
+  // if (num) {
+  //   num++
+  // } else (num == 2) {
+
+  // }
   if (input.value == 1) {
     output.innerText = input.value;
     //컴퓨터 숫자 랜덤 생성&출력
     const correctAnswer = pickRandomNumber();
-    console.log(correctAnswer);
-    input.value = ""
-    output.insertAdjacentText("afterend") = "컴퓨터가 숫자를 뽑았습니다.<br>"
+    console.log(typeof correctAnswer);
+    input.value = "";
+    // output.insertAdjacentText("afterend") = "컴퓨터가 숫자를 뽑았습니다.<br>"
   } else if (input.value == 9) {
     alert("애플리케이션이 종료되었습니다.");
-    input.value = ""
+    input.value = "";
   } else {
     alert("1 혹은 9를 입력해주세요");
-    input.value = ""
+    input.value = "";
   }
 });
 
 function pickRandomNumber() {
-  let numbers = Math.floor(Math.random() * 9) + 1;
-  while (numbers.length <= 3) {
+  let numbers = "";
+
+  while (numbers.length < 3) {
     const num = Math.floor(Math.random() * 9) + 1;
     // 서로 다른 숫자 합치기
+    if (!numbers.includes(num)) {
+      numbers += num;
+    }
   }
+
   return numbers;
 }
