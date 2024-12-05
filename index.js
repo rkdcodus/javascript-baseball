211b70e694cdb7d1cb88f6afb5290cd570fdc9fe
Next-dreamer <mjo2640@gmail.com>
2024-12-05 15:29:03 +0900

    refactor : printHint 함수 조건문 수정

--- index.js
+++ index.js
@@ -33,11 +33,11 @@ const printHint = (ball, strike) => {
 
   if (ball) str += `${ball}볼`;
   if (strike) str += `${strike}스트라이크`;
-  if (str === "") str = "낫싱";
+  if (strike === 0 && ball === 0) str = "낫싱";
 
   print(str);
 
-  if (str === "3스트라이크") {
+  if (strike === NUMBER_LENGTH) {
     print("<br>3개의 숫자를 모두 맞히셨습니다.<br>--------게임 종료---------");
   } else {
     print("<br>숫자를 입력해주세요 : ");
